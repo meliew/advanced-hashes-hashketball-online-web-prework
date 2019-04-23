ea9ebdd1dbd79a8bb4721ce521b8226dc292e7c6
Melissa Williams <melissa@brandmeetscopy.com>
2019-04-23 04:50:43 +0000

    Automatically backed up by Learn

--- hashketball.rb
+++ hashketball.rb
@@ -183,7 +183,7 @@ def big_shoe_rebounds
       end
     end
   end
-  bigfoot[:rebounds]
+  puts bigfoot
 end
 
 #Build a method, `player_numbers`, that takes in an argument of a team name and returns an array of the jersey number's for that team.
